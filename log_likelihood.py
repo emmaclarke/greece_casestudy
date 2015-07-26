f414ec12b0862760a8a0680097c3f77e878987c0
Emma <clarkee8@tcd.ie>
2015-07-26 11:43:05 +0100

    commented out some

--- log_likelihood.py
+++ log_likelihood.py
@@ -8,14 +8,18 @@ greece_filter = lambda *w: 'greece' not in w
 
 #bigrams
 finder = BigramCollocationFinder.from_words(nltk.corpus.genesis.words('/Users/default/Desktop/july15pilot/Greece/IRL_content/clean_irl.txt'))
+#only bigrams that appear 3+ times
+#finder.apply_freq_filter(3)
 #all bigrams which contain Greece
-finder.apply_ngram_filter(greece_filter)
+#finder.apply_ngram_filter(greece_filter)
 #return the 10 ngrams with the hightest PMI 
 print(finder.nbest(bigram_measures.likelihood_ratio, 10))
 
 #trigrams
-finder = TrigramCollocationFinder.from_words(nltk.corpus.genesis.words('/Users/default/Desktop/july15pilot/Greece/IRL_content/clean_irl.txt'))
+#finder = TrigramCollocationFinder.from_words(nltk.corpus.genesis.words('/Users/default/Desktop/july15pilot/Greece/IRL_content/clean_irl.txt'))
+#only trigrams that appear 3+ times
+#finder.apply_freq_filter(3)
 #all trigrams that contain 'greece'
-finder.apply_ngram_filter(greece_filter)
+#finder.apply_ngram_filter(greece_filter)
 #return the 10 ngrams with the hightest PMI 
-print(finder.nbest(trigram_measures.likelihood_ratio, 10))
+#print(finder.nbest(trigram_measures.likelihood_ratio, 10))
